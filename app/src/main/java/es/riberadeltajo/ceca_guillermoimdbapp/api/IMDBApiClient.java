f55ac677fe646542ece9281ec62c3438de4f04fe
Guillermo <gcecaillan@gmail.com>
2025-01-15 16:18:10 +0100

    Funcionalidad de almacenar las Api Key hecha

--- app/src/main/java/es/riberadeltajo/ceca_guillermoimdbapp/api/IMDBApiClient.java
+++ app/src/main/java/es/riberadeltajo/ceca_guillermoimdbapp/api/IMDBApiClient.java
@@ -1,9 +1,5 @@
 package es.riberadeltajo.ceca_guillermoimdbapp.api;
 
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -14,26 +10,10 @@ public class IMDBApiClient {
 
     public static IMDBApiService getApiService() {
         if (apiService == null) {
-            OkHttpClient client = new OkHttpClient.Builder()
-                    .addInterceptor(chain -> {
-                        String apiKey = apiKeyManager.getCurrentKey();
-                        Request modifiedRequest = chain.request().newBuilder()
-                                .addHeader("X-RapidAPI-Key", apiKey)
-                                .addHeader("X-RapidAPI-Host", "imdb-com.p.rapidapi.com")
-                                .build();
-                        return chain.proceed(modifiedRequest);
-                    })
-                    .connectTimeout(30, TimeUnit.SECONDS)
-                    .readTimeout(30, TimeUnit.SECONDS)
-                    .build();
-
-            // Configuración del cliente Retrofit
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
-
             apiService = retrofit.create(IMDBApiService.class);
         }
         return apiService;
